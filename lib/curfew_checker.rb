389552bb21a9d2858c69f6616b462c2e9a8273de
javeriakhan <javeriak1017@gmail.com>
2018-07-11 15:55:56 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,17 +1,27 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if number==11 
+    puts "It is curfew."
 end
 
 def curfew_checker(time)
-  # code goes here
+  if number<11
+    puts "It is almost curfew."
+  else
+    puts "Go to your room."
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if number<11
+    puts "You are okay."
+  elsif 11==11
+    puts "Apparate to your room.
+  else 
+    puts "You cannot be out."
 end
 
 def deluxe_curfew_checker(time)
-  # code goes here
+ curfew=11
+ puts "My curfew is at #{curfew}."
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
